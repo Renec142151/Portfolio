58c0a0b115fc6bb6fbfe2e3d5f8f7b137204a6b6
Renec142151 <lochschmidtrene@emailn.de>
2024-08-01 17:12:57 +0200

    added why me section and Skills section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WhyMeComponent } from './why-me/why-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, WhyMeComponent ],
+  imports: [LandingPageComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/why-me/why-me.component.ts
+++ src/app/main-content/why-me/why-me.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-why-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './why-me.component.html',
   styleUrl: './why-me.component.scss'
 })
 export class WhyMeComponent {
+  showBase = false;
 
 }
