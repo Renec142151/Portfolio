799e59b6370ec40c514d76a25148e914f50b057d
Renec142151 <lochschmidtrene@emailn.de>
2024-08-04 22:01:10 +0200

    added contact-me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent ],
+  imports: [LandingPageComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, ReferencesComponent, ContactMeComponent ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
