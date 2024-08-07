434b4bd825b1986949144b29f13ebdb4933d8f9c
Renec142151 <lochschmidtrene@emailn.de>
2024-08-07 16:04:59 +0200

    setup translate struktur

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,20 @@
 import { Component, inject } from '@angular/core';
+import { TranslationService } from '../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 isMenuOpen = false;
-
+translate = inject(TranslationService)
 toggleMenu (){
   this.isMenuOpen = !this.isMenuOpen
 }
+
+
 }
