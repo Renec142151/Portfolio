7acc24a2ede8ef9f6cc1ae6029ed9f5dfd32843f
Renec142151 <lochschmidtrene@emailn.de>
2024-08-14 12:56:26 +0200

    bugfix form email

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,14 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { TranslationService } from '../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-  translate = inject(TranslationService)
+  ngAfterViewInit(): void {
+    window.scrollTo(0, 0);
+  }
+  translate = inject(TranslationService);
 }
